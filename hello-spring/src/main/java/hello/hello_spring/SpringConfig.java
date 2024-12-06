5b7ad9d8700fb6b046e6856d13b3cda2ac94f956
YEJI KIM <ex_g@hufs.ac.kr>
2024-12-06 21:43:55 +0900

    GDG Spring 실습 코드

---
+++ hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -0,0 +1,38 @@
+package hello.hello_spring;
+
+import hello.hello_spring.aop.TimeTraceAop;
+import hello.hello_spring.repository.*;
+import hello.hello_spring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private final DataSource dataSource;
+    private final EntityManager em;
+
+    public SpringConfig(DataSource dataSource, EntityManager em) {
+        this.dataSource = dataSource;
+        this.em = em;
+    }
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+//         return new MemoryMemberRepository();
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
+    }
+}
